8800cbc7f6815f0741966b510b73efd16aee5042
Dreta <32196824+Dreta@users.noreply.github.com>
2025-12-20 22:57:55 +0800

    feat: add title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import { RootProvider } from 'fumadocs-ui/provider/next'
 import './global.css'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'The BAID Survival Guide',
+    description: 'An unofficial guide to Beijing Academy International Division.'
+}
 
 export default function Layout({ children }: LayoutProps<'/'>) {
     return (
         <html lang="en" suppressHydrationWarning>
+        <head><link href="/img/logo.png" rel="icon" /></head>
         <body className="flex flex-col min-h-screen">
         <RootProvider search={{ enabled: false }} theme={{
             enabled: false,
